2ec46bd8da44b63ba7ff24f600d01dee107175b2
Gabriel Ebanks <167536152+GEbanks@users.noreply.github.com>
2025-04-16 15:03:51 -0400

    project

---
+++ casinoTypeThing/modules/socket.js
@@ -0,0 +1,72 @@
+const { db } = require("./routes");
+routes = require('./routes.js');
+
+function socketH(socket) {
+
+  console.log('a user connected');
+  socket.on('connect', function () {
+    console.log('user connected');
+  });
+
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+  });
+
+  socket.on('message', function (msg) {
+    console.log('message: ' + msg);
+  });
+  
+  function newATTHand() {
+    const suits = {
+      clubs: 'C',
+      diamonds: 'D',
+      hearts: 'H',
+      spades: 'S'
+    };
+  
+    const ranks = [
+      { name: 'ace', short: 'A', value: 1 },
+      { name: '2', short: '2', value: 2 },
+      { name: '3', short: '3', value: 3 },
+      { name: '4', short: '4', value: 4 },
+      { name: '5', short: '5', value: 5 },
+      { name: '6', short: '6', value: 6 },
+      { name: '7', short: '7', value: 7 },
+      { name: '8', short: '8', value: 8 },
+      { name: '9', short: '9', value: 9 },
+      { name: '10', short: '10', value: 10 },
+      { name: 'jack', short: 'J', value: 10 },
+      { name: 'queen', short: 'Q', value: 10 },
+      { name: 'king', short: 'K', value: 10 }
+    ];
+  
+    const cards = [];
+  
+    for (let suitName in suits) {
+      const suitAbbr = suits[suitName];
+      ranks.forEach(rank => {
+        cards.push({
+          name: `${rank.name} of ${suitName.charAt(0).toUpperCase() + suitName.slice(1)}`,
+          img: `/img/cards/${rank.short}${suitAbbr}.png`,
+          value: rank.value,
+          suit: suitName
+        });
+      });
+    }
+
+    for (let i = cards.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [cards[i], cards[j]] = [cards[j], cards[i]];
+    }
+  
+    const hand = cards.slice(0, 4);
+    socket.emit('newHand', hand);
+  }
+  
+  socket.on('newATTHand', () => {
+    newATTHand();
+  });
+
+}
+
+module.exports = { socketH };
